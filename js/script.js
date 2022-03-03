71c2cee7eb69286ff47d744c025e2c1f0bb52ae4
Kassandra Lira <kassandra_lira@hotmail.com>
2022-03-03 18:59:54 -0300

    alteração js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+if (document.getElementById("nome")
+    "estiver preenchido"
+    "campo de email estiver preenchido") {
+    "envie os dados "
+} else {
+    "peca a usuaria que preencha os campos nome e email"
+}
